4d0d856fa85173a9682155316bc5cb5f35e92623
anaabclara <156119021+anaabclara@users.noreply.github.com>
2024-01-29 17:43:38 -0300

    primeiro tsx

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,5 @@ import express from "express";
 const app = express();
 
 app.listen(3000,function() {
-    console.log("Serviço rodando em http://localhost;3000");
+    console.log("Serviço rodando em http://localhost:3000");
 });
