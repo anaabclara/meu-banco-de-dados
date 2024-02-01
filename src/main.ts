65e9776751074496d007429fb488766e264fe684
anaabclara <156119021+anaabclara@users.noreply.github.com>
2024-02-01 17:32:53 -0300

    primeiro usuario criado

--- src/main.ts
+++ src/main.ts
@@ -1,36 +1,40 @@
 import express from "express";
-import {db,firestore} from '../banco de dados/firebase'
+import { db, firestore } from '../banco de dados/firebase'
 
 const app = express();
+app.use(express.json())
 
-app.get('/' , (req,res) =>{
+app.get('/', (req, res) => {
     res.send("Bem vindo a minha primeira API");
 })
 
-app.post("/usuario", async(req,res) => {
-    const usuario = req.body.nome 
+app.post("/usuario", async (req, res) => {
+    const nome = req.body.nome;
+    const email = req.body.email;
+    const telefone = req.body.telefone
 
-    try{
-        const docRef = await firestore.addDoc(firestore.collection(db,'usuarios'),{
-          nome: nome, 
-        })
+    try {
+        
+        const docRef = await firestore.addDoc(firestore.collection(db, 'usuarios'),
+        {
+            nome: nome,
+            email: email,
+            telefone: telefone,
+        });
+        res.send("Usuario adicionado com sucesso " + docRef.id)
 
-        res.send(docRef.id)
-    }   catch (e) {
+    } catch (e) {
         console.log(e)
 
         res.status(500).send(e)
-
     }
-
-
-    })
-}) 
+})
 
 
 
 
 
-app.listen(3000,function() {
+
+app.listen(3000, function () {
     console.log("Serviço rodando em http://localhost:3000");
 });
